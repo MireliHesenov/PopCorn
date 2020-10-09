86e05300cba383532cf2caf54fce7a14d61fbb45
MiraliHasanov <hasanov.tech@gmail.com>
2020-10-09 21:06:38 +0400

    Updated

--- assets/js/cinema.js
+++ assets/js/cinema.js
@@ -1,6 +1,28 @@
 
-base_url = 'https://api.themoviedb.org/3/'
-api_key= '5046236b23dee0f98193884cb9c3502f'
+const base_url = 'https://api.themoviedb.org/3/'
+const api_key = '5046236b23dee0f98193884cb9c3502f'
+const genres = {
+    28 : 'Action',
+    16 : 'Animated',
+    99 : 'Documentary',
+    18 : 'Drama',
+    10751 : 'Family',
+    14 : 'Fantasy',
+    36 : 'History',
+    35 : 'Comedy',
+    10752 : 'War',
+    80 : 'Crime',
+    10402 : 'Music',
+    9648 : 'Mystery',
+    10749 : 'Romance',
+    27 : 'Horror',
+    10770 : 'TV Movie',
+    53 : 'Thriller',
+    37 : 'Western',
+    12 : 'Adventure',
+    878: 'Science Fiction',
+
+}
 
 document.addEventListener('DOMContentLoaded', (e)=>{
  
@@ -8,40 +30,45 @@ document.addEventListener('DOMContentLoaded', (e)=>{
     document.querySelector('#searchForm').addEventListener('submit',(e)=>{
         e.preventDefault()
         let query = document.querySelector('#searchInput').value;
-        getMovies(query);
+        getMovies(query)
        
     })
 
 })
 
-function getMovies(query){
+async function getMovies(query){
 
     fetch(`${base_url}search/movie?api_key=${api_key}&query=${query}`)
-        .then(response => response.json())
+        .then(response =>{
+            if(response.status == 200){
+               return response.json();
+            }
+        })
         .then(response => {
             let movies = response.results;
-            let html =''
-            console.log(movies)
+            let searchResult = document.createElement('ul');
+            searchResult.setAttribute('class','search-result');
+            var html = '';
             movies.forEach(movie => {
                 
-                html += `
-                
-                <div class="movie">
-                    <a onclick="movieSelected(${movie.id})"><img src="https://image.tmdb.org/t/p/w200${movie.poster_path}"  </a>
-                </div>
-                
-                
-                
-                `
-                
+               if(renderMoviePoster(movie) == undefined) return false  
+             searchResult.innerHTML += renderMoviePoster(movie)
+               
+             
+                             
             });
 
-            document.body.innerHTML = html
+            console.log(searchResult)
+            document.querySelector('main').innerHTML = '' ;
+            document.querySelector('main').append(searchResult);
         
         })
         .catch(err => {
             console.log(err);
         });
+
+        document.querySelector('.loader-wrapper').style.display ='none';
+        document.body.style.overflow = 'auto';        
 
 } 
 
@@ -50,11 +77,121 @@ function movieSelected(id){
     window.location = './moviepage.html'
 }
 
+ async function renderMovieSection(section){
+    await fetch(`${base_url}movie/${section}?api_key=${api_key}`)
+        .then(response => {
+            if(response.status == 200){
+               return response.json();
+            }
+        })
+        .then((response) => {
+            let results = response.results
+            let div = document.querySelector(`.${section}`);
+            let glide = div.parentElement.parentElement.classList.value;
+            console.log(glide)
+            let html =''
+            results.forEach(result =>{
+               html += renderMoviePoster(result,'glide__slide')
+               
+            })
+            div.innerHTML =html
+            new Glide(`.${glide}`,{
+                perView:16,
+                type:'carousel',
+                startAt:1.5,
+                autoplay:3000
+                
+            }).mount()
+        })
+        
+      
+
+      
+}
+
+async function upcomingMovieSlider(){
+
+ await   fetch(`${base_url}movie/popular?api_key=${api_key}`)
+    .then(response =>{
+        if(response.status == 200){
+           return response.json();
+        }else{
+            throw `error ${response.status}`
+        }
+    } )
+    .then((response) => {
+        let results = response.results;
+       console.log(results)
+        let div = document.querySelector('.upcoming');
+        let html =''
+        results.slice(0,6).forEach(result =>{
+
+           let slide = document.createElement('li');
+           slide.classList = 'slide glide__slide';
+           slide.setAttribute('onclick',`movieSelected(${result.id})`)
+           slide.style.background = `linear-gradient(180deg,rgba(7,41,86,0.12),rgba(2,9,22)),
+                                    url(https://image.tmdb.org/t/p/w500${result.backdrop_path})  no-repeat `
+           slide.innerHTML = `
+
+           <span>
+               HIGHLIGHTS
+           </span>
+
+           <h2>
+               ${result.title}
+           </h2>
+
+           <span>
+           ${result.genre_ids.slice(0,3).map(id =>
+            genres[id]  ).join(' | ')}
+           </span>
+
+           <span>
+               
+               <svg xmlns="http://www.w3.org/2000/svg" width="37" height="37" viewBox="0 0 37 37">
+                   <g id="Component_1_1" data-name="Component 1 – 1" transform="translate(1 1)">
+                     <circle id="Oval" cx="17.5" cy="17.5" r="17.5" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="2"/>
+                     <path id="Path" d="M.5.063A.3.3,0,0,0,.171.035.328.328,0,0,0,0,.326V14.674a.328.328,0,0,0,.171.291A.3.3,0,0,0,.5,14.937L9.873,7.763a.335.335,0,0,0,0-.526Z" transform="translate(14 10)" fill="#fff"/>
+                   </g>
+                 </svg>
+                 
+              
+
+           Watch the trailer
+           
+         </span>
+           
+           
+           `;
+
+           
+        div.append(slide)
+        })
+        
+        new Glide('.glide1',{
+            type:'carousel',
+            focusAt:'center',
+            perView:1.5,
+            autoplay:3000
+        }).mount()
+       
+    })
+    
+}
+
+
+
 async function getMovie(){
     
+    
+
     let movieId = sessionStorage.getItem('movieId');
   await fetch(`${base_url}movie/${movieId}?api_key=${api_key}`)
-        .then(response => response.json())
+        .then(response => {
+            if(response.status == 200){
+              return  response.json();
+            }
+        })
         .then(  response => {
 
             let movie = response;
@@ -66,7 +203,8 @@ async function getMovie(){
                                                                         url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})  no-repeat `;
             document.querySelector('.movie-header').style.backgroundSize = 'cover';
                                                                     
-            movieTrailerHandler(movie.title)    
+            movieTrailerHandler(movie.title)
+              
         })
         .catch(err => {
             console.log(err);
@@ -75,16 +213,22 @@ async function getMovie(){
   await  getMovieCredits(movieId);
   await  getSimilarMovies(movieId);
 
-  document.querySelector('.loader-wrapper').style.display = 'none';
-  document.body.style.overflow = "auto"
+ 
     
+
+  
+  
 
 }
 
 async function getMovieCredits(id){
 
  await  fetch(`${base_url}movie/${id}/credits?api_key=${api_key}`)
-        .then(response => response.json())
+        .then(response =>{
+            if(response.status == 200){
+               return response.json();
+            }
+        })
         .then(response => {
             let cast = response.cast;
             
@@ -111,11 +255,15 @@ async function getMovieCredits(id){
 
 async function getSimilarMovies(id){    
     await fetch(`${base_url}movie/${id}/similar?api_key=${api_key}`)
-        .then(response => response.json())
         .then(response => {
-            let  movies = response.results
+            if(response.status == 200){
+              return  response.json();
+            }
+        })
+        .then( response => {
+            let  movies =  response.results
             let html = '';
-            movies.slice(0, 6).forEach(movie => {
+         movies.slice(0, 6).forEach(movie => {
             html +=`
             <li>
                 <a onclick="movieSelected(${movie.id})">
@@ -146,7 +294,11 @@ async function movieTrailerHandler(movie){
                    &q=${query}
                    &key=${key}
                    &maxResults=6 `)
-            .then(response => response.json())
+            .then(response =>{
+                if(response.status == 200){
+                   return response.json();
+                }
+            })
             .then(response =>{
                 console.log(response)        
                 let videos = response.items;
@@ -192,6 +344,23 @@ async function movieTrailerHandler(movie){
 
 }
 
+async function pageLoader(functionName){
+
+   await functionName()
+   document.querySelector('.loader-wrapper').style.display ='none';
+    document.body.style.overflow = 'auto';
+
+}
+
+function renderMoviePoster(movie,className = ""){
+    if(movie.poster_path)
+    return  `<li class="${className}">
+    <a onclick="movieSelected(${movie.id})"><img src="https://image.tmdb.org/t/p/w200${movie.poster_path}"  </a>
+    </li> `
+    
+}
+
+
 document.querySelector('#seeMore').addEventListener('click' , (e)=>{
 
     e.preventDefault()
@@ -203,3 +372,9 @@ document.querySelector('#seeMore').addEventListener('click' , (e)=>{
         document.querySelector('#seeMore').innerText = 'See the full cast'
     }
 })
+
+document.querySelector('#rateBtn').addEventListener('click',(e)=>{
+    e.preventDefault()
+    document.querySelector(".reviews > form").style.display = "flex";
+})
+
